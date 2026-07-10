3911d32406cf87a24d207b2d415fe07ff13f51cd
kimjihoon <jihungim500@gmail.com>
2026-07-11 00:25:59 +0900

    [Chore] JPA Auditing 이전 (JobIsBeApplication.java -> JpaConfig.java)

--- src/main/java/com/leets7th/job_is_be/JobIsBeApplication.java
+++ src/main/java/com/leets7th/job_is_be/JobIsBeApplication.java
@@ -3,10 +3,8 @@ package com.leets7th.job_is_be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableCaching
 public class JobIsBeApplication {
 
