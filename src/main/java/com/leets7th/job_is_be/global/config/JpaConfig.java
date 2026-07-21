3e9d3c766942eb92255ad5488a013a6339b4b64d
yukyoungs <syk9848@gmail.com>
2026-07-21 20:33:08 +0900

    Revert "[Fix] JPA Auditing OffsetDateTime DateTimeProvider 적용"
    This reverts commit 15ca516f0284183621f1b9e38291208fd5bd2b14.

--- src/main/java/com/leets7th/job_is_be/global/config/JpaConfig.java
+++ src/main/java/com/leets7th/job_is_be/global/config/JpaConfig.java
@@ -1,21 +1,10 @@
 package com.leets7th.job_is_be.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.time.OffsetDateTime;
-import java.util.Optional;
-
 // BaseEntity의 @CreatedDate, @LastModifiedDate 활성화
-// BaseEntity의 필드 타입이 OffsetDateTime이라, 기본 LocalDateTime 프로바이더 대신 OffsetDateTime을 직접 제공한다.
 @Configuration
-@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+@EnableJpaAuditing
 public class JpaConfig {
-
-    @Bean
-    public DateTimeProvider auditingDateTimeProvider() {
-        return () -> Optional.of(OffsetDateTime.now());
-    }
 }
