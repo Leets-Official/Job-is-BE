41a536b68c5e3be4d64994cab928818e92e6219e
jihoonkim501 <jihungim500@gmail.com>
2026-07-21 22:29:41 +0900

    [Fix] #28 타임스탬프 관련 config 수정

--- src/main/java/com/leets7th/job_is_be/global/config/JpaConfig.java
+++ src/main/java/com/leets7th/job_is_be/global/config/JpaConfig.java
@@ -1,10 +1,21 @@
 package com.leets7th.job_is_be.global.config;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // BaseEntity의 @CreatedDate, @LastModifiedDate 활성화
+// createdAt/updatedAt이 OffsetDateTime이라 기본 DateTimeProvider(LocalDateTime 반환)로는 변환이 안 되어 직접 지정
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class JpaConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
 }
