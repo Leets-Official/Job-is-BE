f8e3d06356f6d14da96cd6ec32116a481abe4bd4
조연준 <whduswns0550@naver.com>
2026-07-21 17:17:51 +0900

    [Fix] #29 JPA Auditing 시간 타입 불일치 수정

--- src/main/java/com/leets7th/job_is_be/global/config/JpaConfig.java
+++ src/main/java/com/leets7th/job_is_be/global/config/JpaConfig.java
@@ -1,10 +1,20 @@
 package com.leets7th.job_is_be.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
 
 // BaseEntity의 @CreatedDate, @LastModifiedDate 활성화
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class JpaConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
 }
